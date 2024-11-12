601e3a2e5b761feba35859d56827acccd166a40e
Nasser Alomri <nasser.omer133@gmail.com>
2024-11-13 00:42:24 +0300

    Update ListTester.java

--- src/main/java/com/example/project/ListTester.java
+++ src/main/java/com/example/project/ListTester.java
@@ -3,27 +3,8 @@ package com.example.project;
 public class ListTester {
 
     public static <T> T mostFrequentElement(List<T> list) {
-        T mfe = null;
-        int max = 0;
         list.findFirst();
-        while (!list.empty()) {
-            T element = list.retrieve();
-            int count = 0;
-            List<T> tempList = new DLL<>();
-            tempList.insert(element);
-            while (!tempList.empty()) {
-                if (tempList.retrieve().equals(element)) {
-                    count++;
-                }
-                tempList.remove();
-            }
-            if (count > max) {
-                max = count;
-                mfe = element;
-            }
-            list.findNext();
-        }
-        return mfe;
+        return list.retrieve();
     }
 
     public static <T> void circularLeftShift(List<T> list, int n) {
@@ -40,11 +21,13 @@ public class ListTester {
 
     public static <T> void reverseCopy(DLL<T> l1, DLL<T> l2) {
         if (l1.empty()) return;
+
         l1.findFirst();
         while (!l1.last()) {
             l1.findNext();
         }
-        while (!l1.first()) {
+
+        while (l1.first()) {
             l2.insert(l1.retrieve());
             l1.findPrevious();
         }
